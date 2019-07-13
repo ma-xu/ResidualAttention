5d4826cb3f309ec9e2f766527aa5c3d87e547258
13952522076 <melody@dynamic-10-125-247-107.dynamic.unt.edu>
2019-07-13 14:02:44 -0500

    adjust_learning_rate error

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def main_worker(gpu, ngpus_per_node, args):
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
             train_sampler.set_epoch(epoch)
-        adjust_learning_rate(optimizer, epoch, args)
+        adjust_learning_rate(optimizer, epoch, args.lr)
 
         # train for one epoch
         train(train_loader, model, criterion, optimizer, epoch, args)
